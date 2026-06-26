671a919ae005b94feb1e5b89d4e11626309cec6c
Boichau93 <phanboichau2493@gmail.com>
2026-06-26 16:44:55 +0700

    Delete AppKhoNhieuLoc_BUILD_READY directory

--- AppKhoNhieuLoc_BUILD_READY/02_CHAY_THU_APP.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-chcp 65001 >nul
-title CHAY THU - APP KHO NHIEU LOC
-echo =========================================
-echo  CHAY THU APP KHO NHIEU LOC
-echo =========================================
-echo.
-npm start
-pause
--- AppKhoNhieuLoc_BUILD_READY/04_DON_DEP_BUILD_CU.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-chcp 65001 >nul
-title DON DEP BUILD - APP KHO NHIEU LOC
-echo Xoa thu muc dist va cache build cu...
-if exist dist rmdir /s /q dist
-if exist .electron-builder rmdir /s /q .electron-builder
-echo Xong.
-pause
